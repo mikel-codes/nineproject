f13781d12a358b7134f58a189177b653a42c832e
iamchidi <mikezone_0906@yahoo.com>
2019-05-27 13:10:02 +0100

    uploaded and corrected

--- nineapp/views.py
+++ nineapp/views.py
@@ -199,7 +199,7 @@ def registration(request):
 
 def activate(request, uidb64, token):
     try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
+        uid = force_text(urlsafe_base64_decode(uidb64)).decode()
         user = User.objects.get(username=uid)
         if account_token.check_token(user, token):
             user.refresh_from_db()
